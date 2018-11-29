fc19ed708aeae5494bfc80028021c1f215525e36
b794 <bojanacjj@yahoo.com>
2018-11-29 19:49:40 +0100

    .

--- ToDo/js.js
+++ ToDo/js.js
@@ -1,15 +1,18 @@
 var text = document.getElementById('newTask');
 var date = document.getElementById('date');
 var time = document.getElementById('time');
-var tasks = [];
+var li = document.createElement("li");
+var a = document.getElementsByTagName('li');
+
+document.getElementById('add').onclick = function(){
+
+  document.getElementById("addNew").style.display = "block";
+
+}
 
 
 document.getElementById('addTask').onclick = function(){
 
-
-  tasks.push(text.value + " " + time.value + " " + date.value);
-  
-  document.getElementById('tasks').innerHTML = tasks;
-
-
+  a.appendChild( date.value + " " + time.value + " " + text.value);
+  document.getElementById('tasks').appendChild(a);
 }
