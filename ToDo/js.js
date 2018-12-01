6238b613113eb3cca5966a3e8cf510557db08371
b794 <bojanacjj@yahoo.com>
2018-12-01 23:33:42 +0100

    js

--- ToDo/js.js
+++ ToDo/js.js
@@ -1,15 +1,20 @@
 var text = document.getElementById('newTask');
 var date = document.getElementById('date');
 var time = document.getElementById('time');
-var a = document.getElementsByTagName('li');
-
 
 document.getElementById('addTask').onclick = function(){
 
   var node = document.createElement("li");
+  node.className = "list";
   var x = document.createTextNode(date.value + " " + time.value + " " + text.value);
   node.appendChild(x);
   document.getElementById('tasks').appendChild(node);
-	document.getElementById('tasks').style.display = "block";
 
+  for (var i = 0; i < node.length; i++) {
+    var span = document.createElement("span");
+    var y = document.createTextNode("x");
+    span.appendChild(y);
+    node.appendChild(span);
+  }
+	document.getElementById('tasks').style.display = "block";
 }
